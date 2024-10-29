875cdb0f6b8b122567b56520ee086da91fabd8ec
Tirette-Hub <nathan.amorison@gmail.com>
2024-10-29 17:56:36 +0100

    updating the unittest max failure traceback diff length

--- exposurescout/tests/test_FileSystemCollector.py
+++ exposurescout/tests/test_FileSystemCollector.py
@@ -18,6 +18,8 @@ from ..core import tools
 import unittest
 import os
 
+unittest.util._MAX_LENGTH=2000
+
 class TestLinFileSystemCollector(unittest.TestCase):
 	@classmethod
 	def setUpClass(self):
